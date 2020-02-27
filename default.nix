a4ce8cb7fdd7f7537d2d2f51fafaa7cb238a3357
Adam Avramov <my.new@email.here>
2020-02-27 12:57:42 +0100

    update Cargo.lock, format default.nix

--- default.nix
+++ default.nix
@@ -1,10 +1,19 @@
-{ rustPlatform, pkgs }: rustPlatform.buildRustPackage rec {
-  pname = "sequence-rs";
-  version = "0.1.0";
-  src = ./.;
-  cargoSha256 = "1nh8kaanhan87z2nf38mnqh51yd7ff0316f2yabg0bpihmp11nyl";
-  verifyCargoDeps = true;
-  buildType = "release";
-  nativeBuildInputs = with pkgs; [ pkgconfig gcc glibc fuse fuse-common openal libsndfile ];
-  RUST_BACKTRACE = 1;
-}
+{ rustPlatform, pkgs }:
+  rustPlatform.buildRustPackage rec {
+    pname = "sequence-rs";
+    version = "0.1.0";
+    src = ./.;
+    cargoSha256 = "1nh8kaanhan87z2nf38mnqh51yd7ff0316f2yabg0bpihmp11nyl";
+    verifyCargoDeps = true;
+    buildType = "release";
+    nativeBuildInputs = with pkgs; [
+      pkgconfig
+      gcc
+      glibc
+      fuse
+      fuse-common
+      openal
+      libsndfile
+    ];
+    RUST_BACKTRACE = 1;
+  }
